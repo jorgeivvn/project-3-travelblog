e1bf33b7a52523038179db50ac4ea402a0cf6ca6
Jorge Rodriguez <j_rod15@msn.com>
2018-02-05 22:20:20 -0600

    Wired title and post components

--- react-travelblog-app/src/PostForm/PostForm.js
+++ react-travelblog-app/src/PostForm/PostForm.js
@@ -5,23 +5,42 @@ import React, { Component } from 'react';
 class PostForm  extends Component {
   constructor(props) {
       super(props);
-      this.state = {};
+      this.state = {
+        newTitle: '',
+        newPostMessage: '',
+      };
+      this.handleUserTitleInput=this.handleUserTitleInput.bind(this);
+      this.handleUserInput=this.handleUserInput.bind(this);
   }
+  handleUserTitleInput (e) {
+    console.log(this);
+    this.setState({
+      newTitle: e.target.value,
+    })
+  }
+
+  handleUserInput (e) {
+    console.log(this);
+      this.setState({
+        newPostMessage: e.target.value,
+      })
+  }
+
   render() {
     return (
       <div className="PostForm">
       <form>
       <div className="form-group">
       <label for="exampleFormControlInput1">Blog Post</label>
-      <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Title of Post"/>
+      <input type="text" className="form-control" id="exampleFormControlInput1" value={ this.state.newTitle } onChange={ this.handleUserTitleInput } placeholder="Title of Post"/>
       </div>
 
       <div className="form-group">
       <label for="exampleFormControlTextarea1">
       </label>
-      <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Write a new post..."></textarea>
+      <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={ this.state.newPostMessage } onChange={ this.handleUserInput } placeholder="Write a new post..."></textarea>
       </div>
-      <button className="btn btn-primary" type="submit">Submit Post</button>
+      <button className="btn btn-primary" type="submit" onClick= { this.writePost }>Submit Post</button>
       </form>
       </div>
     )
